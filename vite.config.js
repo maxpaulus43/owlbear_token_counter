c5befc2ecc6db656455ba5eb043b74483291d6ce
Max Paulus <maxpaulu@amazon.com>
2023-04-06 01:45:39 -0700

    fix background.html again

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "background.html"),
+      },
+    },
+  },
+});
